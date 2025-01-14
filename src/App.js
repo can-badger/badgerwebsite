a42c740f0a144ecfeb8cb0527f5db2afc74d4330
CanKarpatTD <can@thebadger.house>
2025-01-14 23:44:44 +0300

    stayTuned_Activate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import logo from './white_main_clean.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h1 className="color1">Badger House</h1>
+          <h3 className="color2">Your Agency</h3>
+        </div>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We're renewing
         </p>
-        <a
+        <a>
+          Visit us <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.thebadger.house/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          from old site
         </a>
+        </a>
+        
       </header>
     </div>
   );
